3940e242de6cf49e8a8afb30f8f654dd71574831
OujilYoussef <youssef_oujil@hotmail.com>
2021-07-06 00:18:39 +0100

    l2atawch

--- src/app/shared/post.service.ts
+++ src/app/shared/post.service.ts
@@ -12,23 +12,23 @@ export class PostService {
   constructor(private http: HttpClient) { }
 
   getAllPosts(): Observable<Array<PostModel>> {
-    return this.http.get<Array<PostModel>>('http://localhost:8081/api/posts');
+    return this.http.get<Array<PostModel>>('http://localhost:8080/api/posts');
   }
 
   createPost(postPayload: CreatePostPayload): Observable<any> {
-    return this.http.post('http://localhost:8081/api/posts', postPayload);
+    return this.http.post('http://localhost:8080/api/posts', postPayload);
   }
 
   getPost(id: number): Observable<PostModel> {
-    return this.http.get<PostModel>('http://localhost:8081/api/posts/' + id);
+    return this.http.get<PostModel>('http://localhost:8080/api/posts/' + id);
   }
 
   getAllPostsByUser(name: string): Observable<PostModel[]> {
-    return this.http.get<PostModel[]>('http://localhost:8081/api/posts/by-user/' + name);
+    return this.http.get<PostModel[]>('http://localhost:8080/api/posts/by-user/' + name);
   }
 
 
   getAllPostsByDiscussion(id: number): Observable<PostModel[]> {
-    return this.http.get<PostModel[]>('http://localhost:8081/api/posts/by-discussion/' + id);
+    return this.http.get<PostModel[]>('http://localhost:8080/api/posts/by-discussion/' + id);
   }
 }
